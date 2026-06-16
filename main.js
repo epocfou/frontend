c43ed33bd6d48bb86f103ca5ba87a7d0dc4e5935
Jaideep Singh <epocfou@gmail.com>
2026-06-16 23:29:15 +0530

    Test : Week-1 (Guided)

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 async function runit() {
-  const any_maxi = document.getElementById("maxi");
-  const response = await axios.get("http://127.0.0.1:3000/e");
-  any_maxi.innerHTML = response.data;
+  const response = await axios.get("http://127.0.0.1:3000/a");
+  console.log(response.data);
+  arr = response.data;
+  var lis = document.getElementById("ul1");
+  for (var i = 0; i < response.data.length; i++) {
+    var list = document.createElement("li");
+    list.innerHTML = response.data[i];
+    lis.appendChild(list);
+  }
+}
+runit();
+function funct() {
+  var te = document.getElementById("textarea1");
+  axios.post("http://127.0.0.1:3000/b", { value1: te.value });
+  te.value = "";
 }
