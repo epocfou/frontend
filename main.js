b4d1fcf4c92cae575b6b1d0cd5418839d65752fe
Jaideep Singh <epocfou@gmail.com>
2026-06-19 22:26:56 +0530

    Added Jquery; sending query parameters

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-async function runit() {
-  const response = await axios.get("http://127.0.0.1:3000/a");
-  console.log(response.data);
-  arr = response.data;
-  var lis = document.getElementById("ul1");
-  for (var i = 0; i < response.data.length; i++) {
-    var list = document.createElement("li");
-    list.innerHTML = response.data[i];
-    lis.appendChild(list);
-  }
-}
-runit();
-function funct() {
-  var te = document.getElementById("textarea1");
-  axios.post("http://127.0.0.1:3000/b", { value1: te.value });
-  te.value = "";
-}
+$("#our_button").on("click", (e) => {
+  axios
+    .get("http://localhost:3000/koof", {
+      params: {
+        name: "victor",
+      },
+    })
+    .then((res) => {
+      $(".random_name").html(res.data);
+    });
+});
