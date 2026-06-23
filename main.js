55d6981412ea72856a07f12dac010f6b98daa6aa
Jaideep Singh <epocfou@gmail.com>
2026-06-23 23:50:06 +0530

    Printing 5 images every time the button is clicked

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+var innercount = 0;
+
 $("#our_button").on("click", (e) => {
-  axios.get("http://localhost:3000/f", {}).then((res) => {
+  axios.get("http://localhost:3000/g", {}).then((res) => {
     var d = res.data;
     for (var i = 0; i < d.length; i++) {
       var li = document.createElement("li");
-      li.innerHTML = d[i];
+      var image = document.createElement("img");
+      image.src = d[i];
+      li.appendChild(image);
+      innercount++;
+      $("#innercount").html(innercount);
       $("#ul1").append(li);
     }
   });
